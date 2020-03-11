83d1d37bff15fc929892ee9c158390e8f9b371e4
qinrn <3165511966@qq.com>
2020-03-11 16:04:22 +0800

    add： 力扣算法练习

---
+++ src/train/arithmetic/leetcode/L338.java
@@ -0,0 +1,40 @@
+package train.arithmetic.leetcode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//动态规划，位运算
+public class L338 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int []arr = countBits(num);
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	public static int[] countBits(int num) {
+		int len = (Integer.toBinaryString(num)).length();
+		int count = 0;
+		int[] arrys = new int [num + 1];
+
+		for (int i = 0; i <= num; i++) {
+			String str = Integer.toBinaryString(i);
+			while(str.length()!=len) {
+				str = "0" + str;
+			}
+			
+			for (int j = 0; j < str.length(); j++) {
+				if ("1".equals(String.valueOf(str.charAt(j)))) {
+					count += 1;
+				}
+			}
+		
+			arrys[i] = count ; 
+			count = 0;
+		}
+		
+		return arrys;
+	}
+
+}
