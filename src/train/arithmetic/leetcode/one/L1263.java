47a98da7bf68fdc5ee8101bea1d041d97c600317
qinrn <3165511966@qq.com>
2020-07-03 09:34:39 +0800

    add

---
+++ src/train/arithmetic/leetcode/one/L1263.java
@@ -0,0 +1,16 @@
+package train.arithmetic.leetcode.one;
+
+import java.util.Scanner;
+
+public class L1263 {
+	static int N ; 
+	static int M ;
+	static int[][] area ;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		
+		
+		
+	}
+}
